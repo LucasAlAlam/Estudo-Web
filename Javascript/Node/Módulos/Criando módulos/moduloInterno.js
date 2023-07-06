fa934cced0557afc33ed0d5b738e4d3206bc6077
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-06 01:38:41 +0000

    criando módulos

---
+++ Javascript/Node/Módulos/Criando módulos/moduloInterno.js
@@ -0,0 +1,9 @@
+module.exports = {
+    soma(a,b){
+        console.log(a+b)
+    }
+}
+/*A estrutura module.exports serve para mostrar as funções que serão exportadas no módulo
+irá ser criado um objeto dentro dela contendo um método
+vale observar que nesse module.exports, podemos definir métodos que serão usados por outras funções e estes podem conter métodos internos definidos fora do module.exports
+*/
