33238ece0a03ab3d2c103914f70968260bd3fd66
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-07 14:33:56 +0000

    Manipulando argumentos

--- Javascript/Node/argumentos.js
+++ Javascript/Node/argumentos.js
@@ -1,10 +1,20 @@
 //Os argumentos são passados para um array denominado process.argv
-process.argv.forEach((element) => {
-    console.log(element)
-}); //leitura dos argumentos
 
-console.log(process.argv[3]) //printa o terceiro argumento
-/* lembrando que os argumentos estão organizados na seguinte ordem 
+var minimist = require('minimist') //O minimist serve para transformar o array de argumentos em um vetor associativo. Espera argumentos no formato --<nome da associação> = <conteúdo da associação>
+var arg = minimist(process.argv)
+
+console.log(arg['name'])
+
+/*process.argv.forEach((element) => {
+    console.log(element)
+}); 
+
+leitura dos argumentos sem o minimist
+*/
+
+/*console.log(process.argv[3]) printa o terceiro argumento
+
+lembrando que os argumentos estão organizados na seguinte ordem 
 executável do node
 nome do arquivo
 argumento 1
