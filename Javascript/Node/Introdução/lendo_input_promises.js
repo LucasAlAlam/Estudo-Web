5670f6c0ace013c2fe7054b5042fafe23b3a3f2b
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-08 18:15:28 +0000

    inputs com promises

---
+++ Javascript/Node/Introdução/lendo_input_promises.js
@@ -0,0 +1,5 @@
+/*Promisses são métodos de execução de código assincrono alternativos aos callbaaks. Possuem vantagens em relação aos callbacks tradicionais em relação a organização de código. 
+https://medium.com/trainingcenter/entendendo-promises-de-uma-vez-por-todas-32442ec725c2
+Pordemos usar o módulo inquirer para implementar promisses
+*/
+var dump
