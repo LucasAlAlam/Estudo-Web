0a9eb7609710038670a3e1a769783e157baae4c3
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-08 03:47:59 +0000

    lendo inputs

--- Javascript/Node/Introdução/leitura_input.js
+++ Javascript/Node/Introdução/leitura_input.js
@@ -2,7 +2,12 @@ var scanf = require('readline').createInterface({
     input:process.stdin,
     output:process.stdout
 })
-
+/*
+O comando acima faz um require de readline. O método createInterface cria uma interface de entrada e saída de comandos
+var scanf = require('readline').createInterface(
+    process.stdin,
+    process.stdout
+)*/
 scanf.question('Insira um número: ', (insertion) => {
     console.log(insertion)
     scanf.close()
