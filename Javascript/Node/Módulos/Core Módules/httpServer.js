e11464436c34c7c3f98d3d2e7738df33790ee19a
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-13 20:39:09 +0000

    servidor http simples

--- Javascript/Node/Módulos/Core Módules/httpServer.js
+++ Javascript/Node/Módulos/Core Módules/httpServer.js
@@ -2,10 +2,10 @@ const http = require ('http')
 const port = 5000
 
 const server = http.createServer((req,res) => {
-    res.write('Server Node rodando')
-    res.end()
+    res.write('Server Node rodando')    //Envia requisição HTTP com texto puro
+    res.end()   //fim da sessão
 })
 
-server.listen(port, () => {
+server.listen(port, () => { //O servidor irá ouvir uma determinada porta. Isso será importante para o tratamento de eventos
     console.log('servidor rodando na porta '+port)
 } )
