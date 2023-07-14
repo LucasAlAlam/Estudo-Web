275deb4faddba9025fce825ef56d4c3204b393f9
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-14 19:53:10 +0000

    Manipulando Datas

---
+++ PHP/CursoPHPmoderno/Datas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php
+        date_default_timezone_set("America/Sao_Paulo"); //Muda o padrão de UTC para o horário de SP
+        echo "Hoje é dia ". date("d/m/Y") ; //d é minusculo para dia e maiúsculo para dia da semana
+        echo " A hora é ". date("G:i:s T"); //O padrão é o horário UTC
+    ?>
+    </h1>
+</body>
+</html>
