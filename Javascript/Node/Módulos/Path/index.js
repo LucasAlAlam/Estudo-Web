d023db007e9fded7598bedaa696d199d1bcdfa95
LucasAlAlam <lucasalalam@protonmail.com>
2023-07-19 00:25:14 +0000

    Manipulando path de arquivos

---
+++ Javascript/Node/Módulos/Path/index.js
@@ -0,0 +1,7 @@
+const path = require ('path')
+
+const customPath = '/Inicio/seguindo/continue/quasela/chegando/arquivo.pdf'
+
+console.log(path.basename(customPath), path.extname(customPath), path.dirname(customPath))
+
+//Módulo para manusear caminhos de arquivos
